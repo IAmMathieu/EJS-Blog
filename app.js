04fda63f80c470ddfb72258057cb39ef4531c217
AtomicMathO <Math.Oclock-Atome@protonmail.com>
2022-01-21 09:18:11 +0100

    Etape 2
    - Service des fichiers du dossier static
    
    - creation de la route '/'

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.resolve(__dirname, './static')));
 
+app.get('/', (req, res) => {
+    res.sendFile('integration/index.html', {
+        root: __dirname
+    });
+})
+
 
 app.listen(port, () => {
     console.log(`Server listening on: http://localhost:${port}`);
